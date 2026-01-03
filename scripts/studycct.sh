b0597fe35e5837769bf24c10bd06747313634af2
Annapoorni Sanjeev <annapoornisanjeev@gmail.com>
2026-01-03 15:32:23 -0500

    Update script.
    Signed-off-by: Annapoorni Sanjeev <annapoornisanjeev@gmail.com>

--- scripts/studycct.sh
+++ scripts/studycct.sh
@@ -7,7 +7,7 @@ TIMER=10
 # Function to speak using Festival (use CMU voice + SayText)
 say() {
     local msg="$1"
-    echo "(voice_cmu_us_slt_arctic_hts) (SayText \"$msg\")" | festival
+    echo "(voice_cmu_us_slt_arctic_hts) (set! *duration_stretch* 1.2) (SayText \"$msg\")" | festival
 }
 
 # Parse CSV and extract questions
@@ -47,5 +47,16 @@ for i in $(seq $TIMER -1 1); do
     sleep 1
 done
 
+# Map answer letter to answer text
+answer_text=""
+case "$correct_letter" in
+    A) answer_text="$opt_a" ;;
+    B) answer_text="$opt_b" ;;
+    C) answer_text="$opt_c" ;;
+    D) answer_text="$opt_d" ;;
+esac
+
 # Speak answer
 say "The correct answer is: $correct_letter"
+sleep 0.5
+say "$answer_text"
